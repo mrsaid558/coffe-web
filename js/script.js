c50d794f6952c7f3bb22d1087475341ab7b60ba6
mrsaid558 <65372368+mrsaid558@users.noreply.github.com>
2022-02-24 02:44:50 +0200

    these are new edits

--- js/script.js
+++ js/script.js
@@ -48,3 +48,33 @@ function scrollHeader() {
 window.addEventListener("scroll", scrollHeader);
 
 /* change background header */
+
+/* MixUp filter products */
+
+let mixarProducts = mixitup('.products__content', {
+    selectors: {
+        target: '.products__card'
+    },
+    animation: {
+        duration: 300
+    }
+});
+
+// default filter products
+mixerProducts.filter('.delicacies');
+
+const linkProducts = document.querySelectorAll('.products__item');
+
+function activeProducts() {
+    linkProducts.forEach(l => {
+        l.classList.remove('active-product');
+    });
+
+    this.classList.add('active-product');
+}
+
+linkProducts.forEach(l => {
+    l.addEventListener('click', activeProducts)
+})
+
+/* MixUp filter products */
